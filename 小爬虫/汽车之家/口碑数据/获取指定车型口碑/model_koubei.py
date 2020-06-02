8b1afd47d01cad15e9e593a3db41a578ebbf7aef
chenzijie-chenxiaoli-chentong <412037638@qq.com>
2020-06-02 23:07:34 +0800

    1

--- 小爬虫/汽车之家/口碑数据/获取指定车型口碑/model_koubei.py
+++ 小爬虫/汽车之家/口碑数据/获取指定车型口碑/model_koubei.py
@@ -65,9 +65,15 @@ def log_init():
 class Spider:
     def __init__(self):
         # 口碑数据接口 ss:车系ID, p:页数, s:一页返回数据个数最多50
-        self.SeriesUrl = 'https://koubei.app.autohome.com.cn/autov9.1.0/alibi/seriesalibiinfos-pm2-ss3170-st0-p112-s50-isstruct0-o0.json'
+        # st=0:缩略，1：满意，2：不满意，3：空间，4：动力，5：操控，6：油耗，7：舒适性，8：外观，9：内饰，10：性价比，11：满意度
+        # 12：电耗，13：店铺环境，17：能耗，18：
+        self.SeriesUrl = 'https://koubei.app.autohome.com.cn/autov9.1.0/alibi/seriesalibiinfos-pm2-ss3170-st0-p1-s50-isstruct0-o0.json'
         # 口碑详细数据接口 eid=3052096 口碑详情页ID， self.SeriesUrl接口返回
         self.NewEvaluationUrl = 'https://koubeiipv6.app.autohome.com.cn/autov9.13.0/alibi/NewEvaluationInfo.ashx?eid='
+        # 评论
+        'https://koubeiipv6.app.autohome.com.cn/autov9.13.0/news/koubeicomments.ashx?pm=2&koubeiid=1936376&pagesize=20&lastid=0&hot=0'
+        # 数据
+        self.content = dict()
 
     def red_ini(self):
         """读取ini配置文件"""
@@ -95,18 +101,31 @@ class Spider:
         log_init().info(f'车系：{car} 口碑数据获取中...')
         p = 1
         while True:
+            # 解析口碑ID
             url = f'https://koubei.app.autohome.com.cn/autov9.1.0/alibi/seriesalibiinfos-pm2-ss{car}-st0-p{p}-s50-isstruct0-o0.json'
             try:
                 response = self._parse_url(url).json()
             except:
                 log_init().info(f'{url} 口碑数据解析失败!')
                 return
-            koubeis = response.get('result').get('list')
-            if not koubeis:
+            koubeiids = response.get('result').get('list')
+            if not koubeiids:
                 log_init().info(f'车系：{car}车型口碑ID列表获取完成。')
                 return
-            eids = [i['Koubeiid'] for i in koubeis]
-            for eid in eids:
+
+            for koubeiid in koubeiids:
+                self.content[koubeiid['Koubeiid']] = {}
+
+            # 解析口碑详细数据
+            for st in range(1, 11):
+                url = f'https://koubei.app.autohome.com.cn/autov9.1.0/alibi/seriesalibiinfos-pm2-ss{car}-st{st}-p{p}-s50-isstruct0-o0.json'
+
+                response = self._parse_url(url).json()
+                results = response.get('result').get('list')
+                for result in results:
+                    self.content[result['Koubeiid']][result['contents'][0]['structuredname']] = result['contents'][0]['content']
+
+            for eid in self.content.keys():
                 yield eid
             p += 1
 
@@ -123,7 +142,7 @@ class Spider:
         userId = result.get('userId')  # 用户ID
         userName = result.get('userName')  # 用户姓名
         specname = result.get('specname')  # 购买车型
-        boughtprovincename = result.get('boughtprovincename')  # 购买地点
+        boughtprovincename = result.get('boughtprovincename')  # 购买地点(省)
         dealername = result.get('dealername')  # 购买经销商
         boughtdate = result.get('boughtdate')  # 购买时间
         boughtPrice = result.get('boughtPrice')  # 裸车购买价
@@ -141,12 +160,25 @@ class Spider:
 
         brandname = result.get('brandname')  # 品牌名称
         seriesname = result.get('seriesname')  # 车系名称
-        boughtcityname = result.get('boughtcityname')  # 车系名称
+        boughtcityname = result.get('boughtcityname')  # 购买地点(市)
 
-        data = [[userId, userName, model, specid, brandname, seriesname, specname, boughtprovincename, boughtcityname, dealername,
-                 boughtdate, boughtPrice, actualOilConsumption, drivekilometer, spaceScene, powerScene,
-                 maneuverabilityScene, oilScene, comfortablenessScene, apperanceScene, internalScene,
-                 costefficientScene, purpose]]
+        satisfaction_content = self.content[eid]['满意']  # 满意内容
+        Dissatisfied_content = self.content[eid]['不满意']  # 不满意内容
+        space_content = self.content[eid]['空间']  # 空间内容
+        power_content = self.content[eid]['动力']  # 动力内容
+        Control_content = self.content[eid]['操控']  # 操控内容
+        oilScene_content = self.content[eid]['油耗']  # 油耗内容
+        Comfort_content = self.content[eid]['舒适性']  # 舒适性内容
+        Exterior_content = self.content[eid]['外观']  # 外观内容
+        Interior_content = self.content[eid]['内饰']  # 内饰内容
+        Costeffective_content = self.content[eid]['性价比']  # 性价比内容
+
+        data = [[userId, userName, model, specid, brandname, seriesname, specname, boughtprovincename, boughtcityname,
+                 dealername, boughtdate, boughtPrice, actualOilConsumption, drivekilometer, satisfaction_content,
+                 Dissatisfied_content, spaceScene, space_content, powerScene, power_content, maneuverabilityScene,
+                 Control_content, oilScene, oilScene_content, comfortablenessScene, Comfort_content, apperanceScene,
+                 Exterior_content, internalScene, Interior_content,
+                 costefficientScene, Costeffective_content, purpose]]
         return data
 
     def keep_records(self, eid, vali=False):
@@ -173,9 +205,10 @@ class Spider:
             with open("口碑数据.csv", "r", encoding='utf-8', newline="") as f1:
                 reader = csv.reader(f1)
                 if not [row for row in reader]:
-                    k.writerow(['用户ID', '用户姓名', '车系ID', '车型ID', '品牌名称', '车系名称', '购买车型', '购买地点', '购买地点',
-                                '购车经销商', '购买时间', '裸车购买价', '油耗', '目前行驶', '空间', '动力', '操控', '油耗', '舒适性',
-                                '外观', '内饰', '性价比', '购车目的'])
+                    k.writerow(['用户ID', '用户姓名', '车系ID', '车型ID', '品牌名称', '车系名称', '购买车型', '购买地点(省)', '购买地点(市)',
+                                '购车经销商', '购买时间', '裸车购买价', '油耗(百公里)', '目前行驶', '满意内容', '不满意内容', '空间(评分)', '空间内容',
+                                '动力(评分)', '动力内容', '操控(评分)', '操控内容', '油耗(评分)', '油耗内容', '舒适性(评分)', '舒适性内容',
+                                '外观(评分)', '外观内容', '内饰(评分)', '内饰内容', '性价比(评分)', '性价比内容', '购车目的'])
                     k.writerows(data)
                 else:
                     k.writerows(data)
@@ -215,4 +248,5 @@ class Spider:
 if __name__ == '__main__':
     spider = Spider()
     spider.main(10)
+    # spider.get_eid(4502)
 
